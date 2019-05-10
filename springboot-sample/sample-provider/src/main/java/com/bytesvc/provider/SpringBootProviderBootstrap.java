55a9dc1905a44006f104c7335a4db25d1d474591
litong <litong@ybm100.com>
2019-05-10 21:28:50 +0800

    pgsql启动成功

--- springboot-sample/sample-provider/src/main/java/com/bytesvc/provider/SpringBootProviderBootstrap.java
+++ springboot-sample/sample-provider/src/main/java/com/bytesvc/provider/SpringBootProviderBootstrap.java
@@ -1,4 +1,4 @@
-package com.bytesvc.provider.main;
+package com.bytesvc.provider;
 
 import java.io.IOException;
 
@@ -12,8 +12,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = { "com.bytesvc.provider" })
-@EnableAutoConfiguration(exclude = { MongoAutoConfiguration.class }) // 使用文件存储时, 不需要配置mongodb
+/**
+ * 启动类
+ */
+@SpringBootApplication(scanBasePackages = { "com.bytesvc.provider" }, exclude = { MongoAutoConfiguration.class }) // 使用文件存储时, 不需要配置mongodb
 @EnableJpaRepositories(basePackages = "com.bytesvc.provider.springdata", entityManagerFactoryRef = "entityManagerFactory", transactionManagerRef = "jtaTransactionManager")
 @EntityScan(basePackages = { "com.bytesvc.provider.model" })
 public class SpringBootProviderBootstrap {
